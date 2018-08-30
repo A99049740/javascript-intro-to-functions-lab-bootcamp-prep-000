6f7d3345a8a197b2bc43a338d91b55251515af1d
jill zhang <talktojillzhang@gmail.com>
2018-08-30 04:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function shout(string) {
   return string.toUpperCase()
 }
 function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    return ""
+  }
   if (string === "I love you, Grandma."){
     return "I love you, too.""
   }
